beac46cf1ed305f7afa961207cbf4e5747b576b7
PADDYROH <Pat's PC@DESKTOP-PLGUNPC>
2019-09-24 17:37:01 +1000

    changes to contact and css

--- home.php
+++ home.php
@@ -61,9 +61,10 @@ session_start();
                     <!--Main Content-->
 
                     <div class="home-table">
+                        <label id="home-label"><strong>Mission Statement</strong></label>
                         <label id="home-label"><i>"To provide high-quality artworks presented by official artists. We as a company will provide easy shipment and deliveries, catering for all around Australia and internationally.'"</i></label>
                         <br>
-                        <label id="home-label">Browse by theme</label>
+                        <label id="home-label">Browse by Art Style</label>
                         <table>
                             <tr>
                                 <td><img src="images/TjapT-1.jpg" width="200px" height="200px"></td>
@@ -71,20 +72,21 @@ session_start();
                                 <td><img src="images/ThomA-3.jpg" width="200px" height="200px"></td>
                             </tr>
                             <tr>
-                                <td><label id="home-label">Culture</label></td>
-                                <td><label id="home-label">Skulpture</label></td>
+                                <td><label id="home-label">Cultural</label></td>
+                                <td><label id="home-label">Skulptures</label></td>
                                 <td><label id="home-label">Abstract</label></td>
                             </tr>
                         </table>
                         <br>
                         <br>
                         <label id="home-label">Best selling Artist</label>
-                        <label id="home-label"><strong>Ann Thompson</strong></label>
+                        <label id="home-label"><strong>Thomas Tjapaltjarri</strong></label>
+                        <p id="home-label">Thomas Tjapaltjarri is an Australian Aboriginal artist. He and his brothers Warlimpirrnga and Walala have become well known as the Tjapaltjarri Brothers. Tjapaltjarri and his family became known as the last group of Aborigines to come into contact with modern, European society.</p>
                         <table>
                             <tr>
-                                <td><img src="images/ThomA-1.jpg" width="200px" height="200px"></td>
-                                <td><img src="images/ThomA-2.jpg" width="200px" height="200px"></td>
-                                <td><img src="images/ThomA-3.jpg" width="200px" height="200px"></td>
+                                <td><img src="images/TjapT-2.jpg" width="200px" height="200px"></td>
+                                <td><img src="images/TjapT-3.jpg" width="200px" height="200px"></td>
+                                <td><img src="images/TjapT-4.jpg" width="200px" height="200px"></td>
                             </tr>
                         </table>
 
